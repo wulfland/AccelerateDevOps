5161736970cedef35e4b37b1e0ff5d8901f427bc
kaufm <62788609+kaufm@users.noreply.github.com>
2021-08-03 11:20:28 +0200

    Apply suggestions from code review

--- ch3_pull-request/src/app.js
+++ ch3_pull-request/src/app.js
@@ -3,10 +3,10 @@ function toRomanNumerals(num) {
     var roman = '';
     var i;
     for ( i in lookup ) {
-    while ( num >= lookup[i] ) {
-        roman += i;
-        num -= lookup[i];
-    }
+        while ( num >= lookup[i] ) {
+            roman += i;
+            num -= lookup[i];
+        }
     }
     return roman;
 
