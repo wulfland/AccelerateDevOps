9f74efed8e85af4e8b9dde5cbfa4505abda60c2b
Michael Kaufmann <5276337+wulfland@users.noreply.github.com>
2021-10-25 14:06:32 +0200

    Retry GKE deployment

--- ch9_release/destroy-gke.sh
+++ ch9_release/destroy-gke.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 # Set parameters
-GKE_PROJECT=gke-demo
-GKE_CLUSTER=xyz-demo-shop-cluster
+GKE_PROJECT=valid-octagon-330106
+GKE_CLUSTER=xyz-demo-cluster
+GKE_SERVICE=xyz-service-account
 
-# https://cloud.google.com/compute/docs/regions-zones#available
+# gcloud compute zones list
 GKE_ZONE=europe-west3-a
+
+# gcloud compute regions list
+GKE_REGION=europe-west3
 
 gcloud config set project $GKE_PROJECT
 gcloud config set compute/zone $GKE_ZONE
-gcloud config set compute/region $GKE_ZONE
+gcloud config set compute/region $GKE_REGION
 
 # Delete the cluster
 gcloud container clusters delete $GKE_CLUSTER
+
+# Delete service account
+gcloud iam service-accounts delete "$GKE_SERVICE@$GKE_PROJECT.iam.gserviceaccount.com"
--- ch9_release/setup-gke.sh
+++ ch9_release/setup-gke.sh
@@ -5,19 +5,28 @@
 https://console.cloud.google.com/projectselector2/home/dashboard
 
 # Set parameters
-GKE_PROJECT=gke-demo
-GKE_CLUSTER=xyz-demo-shop-cluster
-GKE_SERVICE=xyz-service
+GKE_PROJECT=valid-octagon-330106
+GKE_CLUSTER=xyz-demo-cluster
+GKE_SERVICE=xyz-service-account
 
-# https://cloud.google.com/compute/docs/regions-zones#available
+# gcloud compute zones list
 GKE_ZONE=europe-west3-a
+
+# gcloud compute regions list
+GKE_REGION=europe-west3
 
 gcloud config set project $GKE_PROJECT
 gcloud config set compute/zone $GKE_ZONE
-gcloud config set compute/region $GKE_ZONE
+gcloud config set compute/region $GKE_REGION
 
 # Create a GKE cluster
 gcloud container clusters create $GKE_CLUSTER --num-nodes=1
+
+# Configure kubctl
+gcloud container clusters get-credentials $GKE_CLUSTER
+
+# Should still be empty
+kubectl get pods
 
 # enable API
 gcloud services enable \
@@ -26,13 +35,13 @@ gcloud services enable \
 
 # Create a service account
 gcloud iam service-accounts create $GKE_SERVICE \
-    --display-name "GitHub Deployment"
+    --display-name "GitHub Deployment" \
     --description "Used to deploy from GitHub Actions to GKE"
 
 # Get mail of service account
 gcloud iam service-accounts list
 
-GKE_SVC_MAIL=$GKE_SERVICE@$GKE_PROJECT.iam.gserviceaccount.com
+GKE_SVC_MAIL="$GKE_SERVICE@$GKE_PROJECT.iam.gserviceaccount.com"
 
 # Add roles:
 gcloud projects add-iam-policy-binding $GKE_PROJECT \
@@ -44,8 +53,13 @@ gcloud projects add-iam-policy-binding $GKE_PROJECT \
 # Download JSON
 gcloud iam service-accounts keys create key.json --iam-account=$GKE_SVC_MAIL
 
+# Activate service account
+gcloud auth activate-service-account --key-file key.json
+
 export GKE_SA_KEY=$(cat key.json | base64)
 
+echo ""
 echo "Please create a secret named 'GKE_SA_KEY' in GitHub with the followign content:"
 echo ""
 echo $GKE_SA_KEY
+echo ""
