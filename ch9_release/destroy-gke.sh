98c18b5a1dd6043349e7dd4803eb8e7b3d82f960
Michael Kaufmann <5276337+wulfland@users.noreply.github.com>
2021-10-27 16:46:31 +0200

    Refactor scripts and changed to yaml deployment
    I refactored all the variables in the scripts and changed the deployment
    to use yaml files (Deployment.yml and Service.yml).
    
    I extended the setup.yml to execute the first deployment.

--- ch9_release/destroy-gke.sh
+++ ch9_release/destroy-gke.sh
@@ -3,23 +3,16 @@
 # Set parameters
 GKE_PROJECT=valid-octagon-330106
 GKE_CLUSTER=xyz-demo-cluster
-GKE_SERVICE=xyz-service-account
-
-# gcloud compute zones list
-GKE_ZONE=europe-west3-a
-
-# gcloud compute regions list
+GKE_SERVICE_ACCOUNT=xyz-serviceaccount
 GKE_REGION=europe-west3
 
 gcloud config set project $GKE_PROJECT
-gcloud config set compute/zone $GKE_ZONE
-gcloud config set compute/region $GKE_REGION
 
 # Delete the cluster
 gcloud container clusters delete $GKE_CLUSTER
 
 # Delete service account
-gcloud iam service-accounts delete "$GKE_SERVICE@$GKE_PROJECT.iam.gserviceaccount.com"
+gcloud iam service-accounts delete "$GKE_SERVICE_ACCOUNT@$GKE_PROJECT.iam.gserviceaccount.com"
 
 # Delete repository
-gcloud artifacts repositories delete $GKE_PROJECT
+gcloud artifacts repositories delete $GKE_PROJECT --location $GKE_REGION
--- ch9_release/setup-gke.sh
+++ ch9_release/setup-gke.sh
@@ -5,15 +5,23 @@
 # See https://console.cloud.google.com/projectselector2/home/dashboard
 
 # Set parameters
-GKE_PROJECT=valid-octagon-330106
-GKE_CLUSTER=xyz-demo-cluster
-GKE_SERVICE=xyz-service-account
+export GKE_PROJECT=valid-octagon-330106
+export GKE_CLUSTER=xyz-demo-cluster
+export GKE_APP_NAME=xyz-demo-shop
+export GKE_SERVICE=xyz-service
+export GKE_SERVICE_ACCOUNT=xyz-serviceaccount
+export GKE_DEPLOYMENT_NAME=xyz-demo-shop-deployment
 
-# gcloud compute zones list
-GKE_ZONE=europe-west3-a
+# Get a list of regions:
+# $ gcloud compute regions list
+#
+# Get a list of zones:
+# $ gcloud compute zones list
+export GKE_REGION=europe-west3
+export GKE_ZONE=europe-west3-a
 
-# gcloud compute regions list
-GKE_REGION=europe-west3
+# Just a placeholder for the first deployment
+export GITHUB_SHA=Test
 
 gcloud config set project $GKE_PROJECT
 gcloud config set compute/zone $GKE_ZONE
@@ -25,40 +33,55 @@ gcloud container clusters create $GKE_CLUSTER --num-nodes=1
 # Configure kubctl
 gcloud container clusters get-credentials $GKE_CLUSTER
 
-# Should still be empty
-kubectl get pods
-
 # enable API
 gcloud services enable \
 	containerregistry.googleapis.com \
-	container.googleapis.com
+	container.googleapis.com \
+  artifactregistry.googleapis.com
+
+# Create repository
+gcloud artifacts repositories create $GKE_PROJECT \
+   --repository-format=docker \
+   --location=$GKE_REGION \
+   --description="Docker repository"
 
 # Create a service account
-gcloud iam service-accounts create $GKE_SERVICE \
+gcloud iam service-accounts create $GKE_SERVICE_ACCOUNT \
     --display-name "GitHub Deployment" \
     --description "Used to deploy from GitHub Actions to GKE"
 
 # Get mail of service account
 gcloud iam service-accounts list
 
-GKE_SVC_MAIL="$GKE_SERVICE@$GKE_PROJECT.iam.gserviceaccount.com"
+GKE_SVC_MAIL="$GKE_SERVICE_ACCOUNT@$GKE_PROJECT.iam.gserviceaccount.com"
 
 # Add roles:
 gcloud projects add-iam-policy-binding $GKE_PROJECT \
   --member=serviceAccount:$GKE_SVC_MAIL \
-  --role=roles/container.admin \
   --role=roles/storage.admin \
-  --role=roles/container.clusterViewer
+  --role=roles/container.clusterAdmin \
+  --role=roles/artifactregistry.admin
 
 # Download JSON
 gcloud iam service-accounts keys create key.json --iam-account=$GKE_SVC_MAIL
 
-# Create repository
-gcloud artifacts repositories create $GKE_PROJECT \
-   --repository-format=docker \
-   --description="Docker repository"
+# Build and push the docker image
+docker build --tag \
+  "$GKE_REGION-docker.pkg.dev/$GKE_PROJECT/$GKE_PROJECT/$GKE_APP_NAME:$GITHUB_SHA" \
+  src/Tailwind.Traders.Web/
+gcloud auth configure-docker $GKE_REGION-docker.pkg.dev --quiet
+docker push "$GKE_REGION-docker.pkg.dev/$GKE_PROJECT/$GKE_PROJECT/$GKE_APP_NAME:$GITHUB_SHA"
 
-export GKE_SA_KEY=$(cat key.json | base64)
+# Create deployment
+envsubst < src/Tailwind.Traders.Web/Deployment.yml | kubectl apply -f -
+
+# Create service
+envsubst < src/Tailwind.Traders.Web/Service.yml | kubectl apply -f -
+
+kubectl get service
+echo ""
+echo "Note: if the EXTERNAL-IP is still pending you have to wait and run 'kubectl get service' again to find out the external ip to test the application!"
+echo ""
 
 echo ""
 echo "Please create a secret named 'GKE_SA_KEY' in GitHub with the followign content:"
