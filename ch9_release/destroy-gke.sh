9fc25f97feb5fa2cfdfbb6b203a22aac2cb4b56d
Michael Kaufmann <5276337+wulfland@users.noreply.github.com>
2021-10-27 20:35:37 +0200

    Fix destroy-gke.sh

--- ch9_release/destroy-gke.sh
+++ ch9_release/destroy-gke.sh
@@ -4,12 +4,12 @@
 GKE_PROJECT=valid-octagon-330106
 GKE_CLUSTER=xyz-demo-cluster
 GKE_SERVICE_ACCOUNT=xyz-serviceaccount
-GKE_REGION=europe-west3
+GKE_ZONE=europe-west3-a
 
 gcloud config set project $GKE_PROJECT
 
 # Delete the cluster
-gcloud container clusters delete $GKE_CLUSTER
+gcloud container clusters delete $GKE_CLUSTER --region $GKE_ZONE
 
 # Delete service account
 gcloud iam service-accounts delete "$GKE_SERVICE_ACCOUNT@$GKE_PROJECT.iam.gserviceaccount.com"
