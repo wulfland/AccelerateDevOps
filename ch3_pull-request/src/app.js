a286fb9f66c094198fad0cf23164066ccb5944be
Miguel Gonzalez Losa <losamig@gmail.com>
2024-01-16 18:04:10 +0100

    Update app.js
    Signed-off-by: Miguel Gonzalez Losa <losamig@gmail.com>

--- ch3_pull-request/src/app.js
+++ ch3_pull-request/src/app.js
@@ -1,5 +1,14 @@
 function toRomanNumerals(num) {
-  return num;
+    var lookup = { M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1 };
+    var roman = '';
+    var i;
+    for ( i in lookup ) {
+    while ( num >= lookup[i] ) {
+        roman += i;
+        num -= lookup[i];
+    }
+    }
+    return roman;
 }
 
 var numbers = [3,4,5,13,42,2021];
